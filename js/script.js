74b65557f07567bad96a1630f09f28a686e8e4a4
miriam_b <MiriamBross27@gmail.com>
2022-01-12 13:57:36 +0100

    Added styles to make logo appear and give the site a dynamic look

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const topOfNav = nav.offsetTop; //if you console.log(topOfNav) you get the pixel
 function fixNav() {
     if(window.scrollY >= topOfNav) {
         document.body.style.paddingTop = nav.offsetHeight + "px"; //adds a padding at the top that's exactly as high as your nav bar was; the + px adds the 
-        document.body.classList.add("fixed-nav"); //this adds the fixed class to the body; you could also add it to the nav element if you wanted to
+        document.body.classList.add("fixed-nav"); //this adds the fixed class to the body; if you add it to the body instead of single elements, you can 
+                                                  // use it for every element nested in the body -> see CSS style changes :)
     } else {
         document.body.style.paddingTop = 0;
         document.body.classList.remove("fixed-nav");
